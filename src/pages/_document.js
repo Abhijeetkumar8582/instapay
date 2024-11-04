2c513d2d3605a0efca9882077919d2994415c817
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-11-04 12:04:09 +0530

    change in script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script defer type="text/javascript" src="https://Abhijeet.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=e4326694-6d1d-499b-bdf9-08dcfa807855&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        <script defer type="text/javascript" src="https://EBC.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=e49eb3b4-9d92-4a2a-d62a-08dcfc9a15f1&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
 
       </body>
